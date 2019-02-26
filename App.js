1f513896011285b59bbd41cb945d953b103c19a7
ieydaaaa <48008911+ieydaaaa@users.noreply.github.com>
2019-02-26 23:07:42 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text> Name : Nurul Khalida binti Isa</Text>
+        <Text> Matric No : 1612390 </Text>
       </View>
     );
   }
